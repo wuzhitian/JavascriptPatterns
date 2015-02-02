ba692f36c73b03198de02bdbac5298594c49159a
wuzhitian <linewu@yeah.net>
2015-02-02 11:02:18 +0800

    Sandbox()

--- chapter5/chapter5.js
+++ chapter5/chapter5.js
@@ -76,7 +76,7 @@
 // 		console.log(name);
 // 	}
 // }
-// Gadget.prototype = (function(){
+// Gadget.prototype = (function(){		//prototype对象为引用类型，字面量重写时要格外注意。字面量重写会将prototype彻底指向另一个对象
 // 	var browser0 = "ffffox";
 // 	return {
 // 		getBrowser: function(){
@@ -95,3 +95,193 @@
 // var kkey = new Gadget("kkey");
 // toy.getBrowser();				//ffffox
 // ********************************************************//
+//
+//P98 将私有方法揭示为 公共方法
+// ********************************************************//
+// var myArray;
+// (function(){
+// 	function isArray(a){
+// 		return Object.prototype.toString.call(a) == "[object Array]";
+// 	}
+// 	function indexOf(haystack, needle){
+// 		var max = haystack.length;
+// 		for(var i = 0; i < max; i++){
+// 			if(haystack[i] === needle){
+// 				return i;
+// 			}
+// 		}
+// 		return -1;
+// 	}
+// 	myArray = {		//对象引用私有类型变量,即使在外部重写了键名指向，也不会改变键名原指向的方法
+// 		isArray: isArray
+// 		,indexOf: indexOf
+// 		,inArray: indexOf
+// 	}
+// }());
+// console.log(myArray.isArray([1,2]));					//true
+// console.log(myArray.isArray({0:1}));					//false
+// console.log(myArray.indexOf(["a", "b", "z"], "z"));		//2
+// console.log(myArray.inArray(["a", "b", "z"], "z"));		//2
+
+// myArray.indexOf = null;
+// console.log(myArray.inArray(["a", "b", "z"], "z"));		//2
+// ********************************************************//
+// 
+// P99 模块模式			在局部作用域内声明，封装对象，将对象当函数执行结果返回赋值给变量
+// ********************************************************//
+// var MYAPP = {};
+// MYAPP.namespace = function(str){
+// 	var tempArr = str.split(".");
+// 	var parent = MYAPP;
+// 	var i = tempArr[0] == "MYAPP"? 1 : 0;
+// 	for(; i < tempArr.length; i++){
+// 		if(typeof parent[tempArr[i]] == "undefined"){	//
+// 			parent[tempArr[i]] = {};
+// 		}
+// 		parent = parent[tempArr[i]];
+// 	}
+// 	return parent;
+// }
+// MYAPP.namespace('MYAPP.utilities.array');
+// MYAPP.utilities.array = (function(){
+// 	var array_string = "[object Array]";
+// 	var ops = Object.prototype.toString;
+// 	return {
+// 		inArray: function(needle, haystack){
+// 			for(var i = 0; max = haystack.length; i < max; i++){
+// 				if(haystack[i] === needle){
+// 					return true;
+// 				}
+// 			}
+// 		}
+// 		,isArray: function(a){
+// 			return ops.call(a) === array_string;
+// 		}
+// 	}
+// }());
+// ********************************************************//
+// 
+// P101 揭示模块模式		//类似于“将私有方法揭示为公共方法”
+// ********************************************************//
+// var MYAPP = {};
+// MYAPP.namespace = function(str){
+// 	var tempArr = str.split('.');
+// 	var parent = MYAPP;
+// 	var i = tempArr[0] == "MYAPP" ? 1 : 0;
+// 	for(; i < tempArr.length; i++){
+// 		if(typeof parent[tempArr[i]] == "undefined"){
+// 			parent[tempArr[i]] = {};
+// 		}
+// 		parent = parent[tempArr[i]];
+// 	}
+// 	return parent;
+// }
+
+// MYAPP.utilities.array = (function(){
+// 	//私有属性
+// 	var array_string = "[object Array]";
+// 	var ops = Object.prototype.toString;
+// 	var inArray = function(haystack, needle){
+// 		for(var i = 0; i < haystack.length; i++){
+// 			if(haystack[i] === needle){
+// 				return i;
+// 			}
+// 		}
+// 		return -1;
+// 	};
+// 	var isArray = function(a){
+// 		return ops.call(a) === array_string;
+// 	};
+// 	return {
+// 		isArray: isArray
+// 		,indexOf: inArray
+// 	};
+// }());
+// ********************************************************//
+// 
+// P102 创建构造函数的模块
+// ********************************************************//
+// var MYAPP = {};
+// MYAPP.namespace = function(str){
+// 	var tempArr = str.split(".");
+// 	var parent = MYAPP;
+// 	var i = tempArr[0] == "MYAPP" ? 1 : 0;
+// 	for(; i < tempArr.length; i++){
+// 		if(typeof parent[tempArr[i]] == 'undefined'){
+// 			parent[tempArr[i]] = {}
+// 		}
+// 		parent = parent[tempArr[]i];
+// 	}
+// 	return parent;
+// }
+
+// MYAPP.namespace('MYAPP.utilities.Array');
+// MYAPP.utilities.Array = (function(){
+// 	//依赖
+// 	var uobj = MYAPP.utilities.object;
+// 	var ulang = MYAPP.utilities.lang;
+// 	var Constr = function(o){
+// 		this.elements = this.toArray(o);
+// 	};
+// 	Constr.prototype = {
+// 		constructor: MYAPP.utilities.Array,
+// 		toArray: function(obj){
+// 			var a = [];
+// 			for(var i = 0; i < obj.length; i++){
+// 				a[i] = obj[i];
+// 			}
+// 			return a;
+// 		}
+// 	};
+// 	//返回要分配给新命名空间的构造函数
+// 	return Constr;
+// }());
+// ********************************************************//
+// 
+// P106 沙箱模式 实现构造函数				???没看懂,不知道说的是啥???
+// ********************************************************//
+// function Sandbox(){
+// 	//将参数转换成一个数组
+// 	var args = Array.prototype.slice.call(arguments);
+// 	//最后一个参数是回调函数
+// 	var callback = args.pop();
+// 	//模块可以作为一个数组传递，或作为单独的参数传递,
+// 	var modules = (args[0] && typeof args[0] === "string") ? args : args[0];
+// 	var i;
+// 	//确保该函数作为构造函数被调用
+// 	if(!(this instanceof Sandbox)){
+// 		return new Sandbox(modules, callback);
+// 	}
+// 	//需要向'this'添加的属性
+// 	this.a = 1;
+// 	this.b = 2;
+// 	//现在向该核心'this'对象添加模块
+// 	//不指定模块名称或指定'*'都表示"使用所有模块"
+// 	if(!modules || modules === '*'){
+// 		modules = [];
+// 		for(i in Sandbox.modules){
+// 			if(Sandbox.modules.hasOwnProperty(i)){
+// 				modules.push(i);
+// 			}
+// 		}
+// 	}
+// 	//初始化所需的模块
+// 	for( i = 0; i < modules.length; i++){
+// 		Sandbox.modules[modules[i]](this);
+// 	}
+// 	callback(this);
+// }
+// //需要的任何原型属性
+// Sandbox.prototype = {
+// 	name: "My Application"
+// 	,version: "1.0"
+// 	,getName: function(){
+// 		return this.name;
+// 	}
+// }
+// ********************************************************//
+// 
+// 
+// ********************************************************//
+// ********************************************************//
+
--- note.js
+++ note.js
@@ -2,10 +2,12 @@
 // var array = [1,2,3,4,5];
 // for(var i=array.length;i>0;i--) array[i]=array[i-1];
 // console.log(array);	//[ 1, 1, 2, 3, 4, 5 ]
+// ********************************************************//
+
 
 // var cat = '{"name": "fido", "dob": new Function("new Date()")(), "legs": "[1, 2, 3, 4]" }';
 // console.log(JSON.parse(cat));
-// console.log()
+// ********************************************************//
 
 // var dog = {
 // 	name: "fido", 
@@ -15,6 +17,8 @@
 // var jsonstr = JSON.stringify(dog);
 // console.log(jsonstr);	//{"name":"fido","dob":"2014-07-31T16:32:44.105Z","legs":[1,2,3,4]};
 // console.log(typeof jsonstr);	//string
+// ********************************************************//
+
 
 
 //其实很简单，将像arguments那种的类数组对象转换为数组的形式。
@@ -37,31 +41,70 @@
 //      console.log(this instanceof String);    // true
 // }
 // a.call(123);
+// ********************************************************//
 
 
 
 //关于给构造函数定义了prototype后的变化
-function AAA(){
-	var name = "AAANAME";
-	this.getName = function(){
-		console.log(name);
-	}
-}
-var aaa0 = new AAA();
+// function AAA(){
+// 	var name = "AAANAME";
+// 	this.getName = function(){
+// 		console.log(name);
+// 	}
+// }
+// var aaa0 = new AAA();
 
-AAA.prototype = (function(){
-	var val = "YYYYYYYYY";
-	return {
-		getVal: function(){
-			console.log(val);
-		}
-	}
-})();
-var aaa1 = new AAA();
-aaa1.getName();								//AAANAME
-aaa0.getName();								//AAANAME
-aaa1.getVal();								//YYYYYYYYY
-// aaa0.getVal();							//Error: Object #<AAA> has no method 'getVal'
-console.log(aaa1.constructor);				//[Function: Object]
-console.log(aaa0.constructor === AAA);		//true
-console.log(aaa1.constructor === AAA);		//false
+// AAA.prototype = (function(){
+// 	var val = "YYYYYYYYY";
+// 	return {
+// 		getVal: function(){
+// 			console.log(val);
+// 		}
+// 	}
+// })();
+// var aaa1 = new AAA();
+// aaa1.getName();								//AAANAME
+// aaa0.getName();								//AAANAME
+// aaa1.getVal();								//YYYYYYYYY
+// // aaa0.getVal();							//Error: Object #<AAA> has no method 'getVal'
+// console.log(aaa1.constructor);				//[Function: Object]
+// console.log(aaa0.constructor === AAA);		//true
+// console.log(aaa1.constructor === AAA);		//false
+// ********************************************************//
+
+
+
+//构造函数的prototype方法是允许被重写的，重写构造函数的prototype时，要确定自己在做什么
+// function Person(name,age){
+// 	this.name = name;
+// 	this.age = age;
+// }
+// // Person.prototype.say = function(){
+// // 	console.log(this.name);
+// // }
+// Person.prototype = (function(){		//字面量重写prototype
+// 	return {
+// 		constructor: Person,
+// 		say: function(){
+// 			console.log(this.name);
+// 		}
+// 	}
+// }());
+// var Ken = new Person("Ken", 25);
+// var Jean = new Person("Jean", 15);
+// Ken.say();
+// Jean.say();
+// Ken.constructor.prototype.say = function(){
+// 	console.log(this.age);
+// }
+// Ken.say();
+// Jean.say();
+// ********************************************************//
+
+// if(typeof aa == "undefined"){	//此处表明undefined是一种“数据类型”
+// 	console.log("HHHH");
+// }
+// if(aa=="undefined"){			
+// 	console.log("KKKKKK")
+// }
+// ********************************************************//
